57ea0f418b1ad7457376ad7cb31b8b42635a64d6
Petros Angelatos <petrosagg@gmail.com>
2017-07-09 01:20:47 -0700

    implement signature generation for both blake2 and md4
    Signed-off-by: Petros Angelatos <petrosagg@gmail.com>

---
+++ signature.go
@@ -0,0 +1,78 @@
+package rdiff
+
+import (
+	"io"
+	"fmt"
+	"encoding/binary"
+
+	"golang.org/x/crypto/md4"
+	"golang.org/x/crypto/blake2b"
+)
+
+const (
+	BLAKE2_SUM_LENGTH = 32
+	MD4_SUM_LENGTH = 16
+)
+
+func Signature(input io.Reader, output io.Writer, blockLen, strongLen uint32, sigType MagicNumber) error {
+	var maxStrongLen uint32
+
+    switch sigType {
+    case BLAKE2_SIG_MAGIC:
+		maxStrongLen = BLAKE2_SUM_LENGTH
+    case MD4_SIG_MAGIC:
+		maxStrongLen = MD4_SUM_LENGTH
+    default:
+        return fmt.Errorf("invalid sigType %#x", sigType);
+    }
+
+	if strongLen > maxStrongLen {
+		return fmt.Errorf("invalid strongLlen %d for sigType %#x", strongLen, sigType);
+	}
+
+	err := binary.Write(output, binary.BigEndian, sigType)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(output, binary.BigEndian, blockLen)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(output, binary.BigEndian, strongLen)
+	if err != nil {
+		return err
+	}
+
+	block := make([]byte, blockLen)
+
+	for {
+		n, err := io.ReadFull(input, block)
+		if err == io.ErrUnexpectedEOF || err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+		data := block[:n]
+
+		err = binary.Write(output, binary.BigEndian, WeakChecksum(data))
+		if err != nil {
+			return err
+		}
+
+		var strong []byte
+
+		switch sigType {
+		case BLAKE2_SIG_MAGIC:
+			d := blake2b.Sum256(data)
+			strong = d[:strongLen]
+		case MD4_SIG_MAGIC:
+			d := md4.New()
+			d.Write(data)
+			strong = d.Sum(nil)[:strongLen]
+		}
+
+		output.Write(strong)
+	}
+
+	return nil
+}
